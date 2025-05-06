09c8835d5fce46666acebc61f655296d0ef1616a
ashrith-77 <ashrithr.15@gmail.com>
2025-05-06 15:41:02 +0530

    cards hover effect

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -37,22 +37,22 @@ const page = () => {
                     <h1 className='text-5xl font-extrabold text-[#2E1F1F] leading-tight text-center my-8'>Why Choose Us</h1>
                     <p className='text-[#2E1F1F] my-6 leading-relaxed font-medium text-center'>Elevating Your Home Buying Experience with Expertise, Integrity, and Unmatched Personalized Service</p>
                     <div className='flex flex-wrap justify-center gap-8 px-6 py-10 '>
-                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center'>
+                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center transition-transform hover:scale-105'>
                             <img src="images/Group 33.png"></img>
                             <h4 className='text-xl font-semibold text-[#2E1F1F] mt-4 mb-2'>Expert Guidance</h4>
                             <p>Benefit from our team's seasoned expertise for a smooth buying experience</p>
                         </div>
-                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center'>
+                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center transition-transform hover:scale-105'>
                             <img src="images/Group 34.png"></img>
                             <h4 className='text-xl font-semibold text-[#2E1F1F] mt-4 mb-2'>Personalized Service</h4>
                             <p>Our services adapt to your unique needs, making your journey stress-free</p>
                         </div>
-                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center'>
+                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center transition-transform hover:scale-105'>
                             <img src="images/Group 35.png"></img>
                             <h4 className='text-xl font-semibold text-[#2E1F1F] mt-4 mb-2'>Transparent Process</h4>
                             <p>Stay informed with our clear and honest approach to buying your home</p>
                         </div>
-                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center'>
+                        <div className=' flex flex-col items-center bg-[#FEF7F2] shadow-md rounded-lg p-6 max-w-xs text-center transition-transform hover:scale-105'>
                             <img src="images/Group 36.png"></img>
                             <h4 className='text-xl font-semibold text-[#2E1F1F] mt-4 mb-2'>Exceptional Support</h4>
                             <p>Providing peace of mind with our responsive and attentive customer service</p>
