bdb4b184a33a2fee25686be0d2fb982404b81e20
ashrith-77 <ashrithr.15@gmail.com>
2025-09-06 16:17:26 +0530

    .

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import TextType from '@/components/TextType';
 const Hero = () => {
   return (
     <div
       className="h-[700px] bg-cover bg-center flex flex-col justify-center items-start text-left px-10"
       style={{ backgroundImage: 'url("/images/images.png")' }}
     >
+    <TextType/>
       <h1 className="text-5xl font-bold text-[#2B1B12] mb-4">
         Welcome to NestQuest
       </h1>
